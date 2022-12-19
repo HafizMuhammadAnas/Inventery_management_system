42d36882ad77087ac4e73e5d494cd49fdfed8e70
HafizMuhammadAnas <hafizanas3838@gmail.com>
2022-12-19 16:17:05 +0500

    forth

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -1,9 +1,17 @@
 class UsersMailer < ApplicationMailer
   def send_greetings_notification(user)
+    create_reset_password_token(user)
     @user = user
 
-    mail to: "hafizanas3838@gmail.com",
+    mail to: user.email,
     subject: "Book Borrowed"
 
+  end
+  def create_reset_password_token(user)
+    raw, hashed = Devise.token_generator.generate(User, :reset_password_token)
+    @token = raw
+    user.reset_password_token = hashed
+    user.reset_password_sent_at = Time.now.utc
+    user.save
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :mail
   require 'csv'
 
   has_many :orders
@@ -6,7 +7,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
+
+
   def self.import(file)
     records = CSV.foreach(file, headers: true) do |row|
       user = User.new(row.to_hash)
@@ -15,5 +18,11 @@ class User < ApplicationRecord
       #   ApplicationMailer.send_welcome(user).deliver
       # end
     end
+
+  end
+  def mail
+    # UsersMailer.send_greetings_notification(self)
+    UsersMailer.send_greetings_notification(self).deliver_now
+
   end
 end
